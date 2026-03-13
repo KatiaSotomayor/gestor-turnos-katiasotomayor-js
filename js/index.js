a8ecab0b0642c9184ab43e98cddceb600296da79
Katia Sotomayor <katiasotomayor042@gmail.com>
2026-03-12 21:22:15 -0300

    Arreglado del JSON y configuracion para que pushee los datos del form al JSON

--- js/index.js
+++ js/index.js
@@ -7,54 +7,49 @@ fetch(url)
 
         const select_nom_ape = document.getElementById("select-nombre-apellido");
 
+        
         //AUTOLLENADO DE <OPTION> PARA QUE SOLO SE PUEDAN ELEGIR A LOS EMPLEADOS EXISTENTES PARA AÑADIRLE UN TURNO
-        json.empleados.forEach (empleado => {          
+        json.empleados.forEach(empleado => {
             select_nom_ape.innerHTML += `
-            <option value="${empleado. id_trabajador}">${empleado.nom_ape}</option>
+            <option value="${empleado.nom_ape}">${empleado.nom_ape}</option>
             `;
         });
+
+        const formulario = document.getElementById("form-turnos")
+
+        //TOMA DE VALORES DEL FORM AL HACER CLICK EN EL BTN SUBMIT
+        formulario.addEventListener("submit", function (event) {
+            event.preventDefault()
+
+            const v_nombre_ape = document.getElementById("select-nombre-apellido").value
+            const v_fecha = document.getElementById("fecha").value
+            const v_entrada = document.getElementById("horario-entrada").value
+            const v_salida = document.getElementById("horario-salida").value
+            const v_mes = document.getElementById("mes").value.toLowerCase();
+
+            //ALMACENAMIENTO DE DATOS EN UNA VARIABLE
+            const turno = {
+                nom_ape: v_nombre_ape,
+                dia: v_fecha,
+                entrada: v_entrada,
+                salida: v_salida,
+            }
+
+            //SE AGREGA A LA LISTA Y SE AGREGA EL NUEVO TURNO AL HTML
+            json.turnos[v_mes].push(turno)
+            agregar_al_html(turno)
+
+            //Y SE LIMPIA EL FORM
+            formulario.reset()
+        });
+
+
+        //FUNCION PARA AGREGAR EL NUEVO TURNO A HTML
+        function agregar_al_html(turno) {
+            const turno_agregado = document.getElementById("dia" + turno.dia)
+
+            turno_agregado.innerHTML += `
+        <hr><li>${turno.nom_ape}: De ${turno.entrada} a ${turno.salida}</li>
+        `;
+        }
     });
-
-    
-
-//LISTA PARA ALMACENAR TURNOS
-let turnos = []
-
-const formulario = document.getElementById("form-turnos")
-
-//TOMA DE VALORES DEL FORM AL HACER CLICK EN EL BTN SUBMIT
-formulario.addEventListener("submit", function (event) {
-    event.preventDefault()
-
-    const nom_ape = document.getElementById("nombre-apellido").value
-    const fecha = document.getElementById("fecha").value
-    const entrada = document.getElementById("horario-entrada").value
-    const salida = document.getElementById("horario-salida").value
-    const mes = document.getElementById("mes").value
-
-    //ALMACENAMIENTO DE DATOS EN UNA VARIABLE
-    const turno = {
-        nombre: nom_ape,
-        fecha: fecha,
-        entrada: entrada,
-        salida: salida,
-        mes: mes,
-    }
-
-    //SE AGREGA A LA LISTA Y SE AGREGA EL NUEVO TURNO AL HTML
-    turnos.push(turno)
-    agregar_al_html(turno)
-
-    //Y SE LIMPIA EL FORM
-    formulario.reset()
-});
-
-
-//FUNCION PARA AGREGAR EL NUEVO TURNO A HTML
-function agregar_al_html(turno) {
-    const turno_agregado = document.getElementById("dia" + turno.fecha)
-
-    turno_agregado.innerHTML += `
-    <hr><li>${turno.nombre}: De ${turno.entrada} a ${turno.salida}</li>
-    `;
-}
