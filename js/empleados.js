f5de31d41c7fef4552f6a065e5e07d171f29183a
Katia Sotomayor <katiasotomayor042@gmail.com>
2026-03-17 18:20:15 -0300

    Funcion constructora añadida

---
+++ js/empleados.js
@@ -0,0 +1,29 @@
+//iMPORTAR JSON CON LA INFO DE LOS EMPLEADOS
+const url = "../db/data.json"
+fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json);
+
+        //AUTORRELLENADO CON DATOS YA CARGADOS DE LOS EMPLEADOS
+        const contenedor = document.getElementById("recuadro-turnos");
+        json.empleados.forEach((empleados, incremento) => {
+            contenedor.innerHTML += `
+                <ul id="empleado${incremento + 1}" class="dia">
+                    <h3>${empleados.nom_ape}</h3>
+                    <li>Puesto: ${empleados.puesto}</li>
+                    <li>Área: ${empleados.area}</li>
+                    <li>Estado: ${empleados.estado}</li>
+                </ul>
+            `;
+        });
+    })
+    .catch(error => {
+        Swal.fire({
+            icon: "error",
+            title: "Error",
+            text: error,
+            confirmButtonColor: 'rgb(143, 173, 136)'
+        });
+    }
+    )
--- js/turnos.js
+++ js/turnos.js
@@ -1,5 +1,14 @@
 //iMPORTAR JSON CON LA INFO DE LOS EMPLEADOS
 const url = "../db/data.json"
+
+function f_turno(nom_ape, dia, entrada, salida) {
+    this.nom_ape = nom_ape;
+    this.dia = dia;
+    this.entrada = entrada;
+    this.salida = salida;
+}
+
+
 fetch(url)
     .then(response => response.json())
     .then(json => {
@@ -29,28 +38,25 @@ fetch(url)
 
             const label_ultima_mod = document.getElementById('label_ultima_mod');
 
-            if ((v_fecha > 29 && v_mes == "febrero") || (v_fecha > 30 && (v_mes=="abril" || v_mes=="junio" || v_mes=="septiembre" || v_mes=="noviembre"))) {
+            if ((v_fecha > 29 && v_mes == "febrero") || (v_fecha > 30 && (v_mes == "abril" || v_mes == "junio" || v_mes == "septiembre" || v_mes == "noviembre"))) {
                 Swal.fire({
-                            icon: "error",
-                            title: "Error: Debe elegir un día válido en el mes elegido. Por favor, vuelva a intentar",
-                            confirmButtonColor: 'rgb(143, 173, 136)'
-                        });
+                    icon: "error",
+                    title: "Error: Debe elegir un día válido en el mes elegido. Por favor, vuelva a intentar",
+                    confirmButtonColor: 'rgb(143, 173, 136)'
+                });
 
             } else {
                 //ALMACENAMIENTO DE DATOS EN UNA VARIABLE
-                const turno = 
-                {
-                    nom_ape: v_nombre_ape,
-                    dia: v_fecha,
-                    entrada: v_entrada,
-                    salida: v_salida,
-                }
+                const turno = new f_turno(
+                    v_nombre_ape,
+                    v_fecha,
+                    v_entrada,
+                    v_salida
+                );
 
-                
                 label_ultima_mod.innerText = `Agregando..`;
-
                 //SE AGREGA A LA LISTA Y SE AGREGA EL NUEVO TURNO AL HTML
-                let cargando = setTimeout (() => {
+                let cargando = setTimeout(() => {
                     try {
                         //SE INTENTA AGREGAR AL HTML
                         json.turnos[v_mes].push(turno)
@@ -73,9 +79,9 @@ fetch(url)
                             confirmButtonColor: 'rgb(143, 173, 136)'
                         });
                     } finally {
-                        
+
                     }
-                    
+
                 }, 2000)
 
                 //FUNCION PARA AGREGAR EL NUEVO TURNO A HTML
@@ -87,13 +93,13 @@ fetch(url)
                     `;
                 }
 
-                function modificacion_date () {
+                function modificacion_date() {
                     let fecha_mod = new Date();
                     label_ultima_mod.innerText = `Ultima modificación: ${fecha_mod}`;
-                }           
+                }
             }
 
-             
+
         });
 
     })
