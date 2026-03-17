eb2e8f2412894209b58c1b7dd4c3ff203deff6e9
Katia Sotomayor <katiasotomayor042@gmail.com>
2026-03-17 17:05:43 -0300

    Agregado de validación en las entradas de meses, y agregado de los meses en el html

--- js/turnos.js
+++ js/turnos.js
@@ -27,52 +27,74 @@ fetch(url)
             const v_salida = document.getElementById("horario-salida").value
             const v_mes = document.getElementById("mes").value.toLowerCase();
 
-            //ALMACENAMIENTO DE DATOS EN UNA VARIABLE
-            const turno = {
-                nom_ape: v_nombre_ape,
-                dia: v_fecha,
-                entrada: v_entrada,
-                salida: v_salida,
+            const label_ultima_mod = document.getElementById('label_ultima_mod');
+
+            if ((v_fecha > 29 && v_mes == "febrero") || (v_fecha > 30 && (v_mes=="abril" || v_mes=="junio" || v_mes=="septiembre" || v_mes=="noviembre"))) {
+                Swal.fire({
+                            icon: "error",
+                            title: "Error: Debe elegir un día válido en el mes elegido. Por favor, vuelva a intentar",
+                            confirmButtonColor: 'rgb(143, 173, 136)'
+                        });
+
+            } else {
+                //ALMACENAMIENTO DE DATOS EN UNA VARIABLE
+                const turno = 
+                {
+                    nom_ape: v_nombre_ape,
+                    dia: v_fecha,
+                    entrada: v_entrada,
+                    salida: v_salida,
+                }
+
+                
+                label_ultima_mod.innerText = `Agregando..`;
+
+                //SE AGREGA A LA LISTA Y SE AGREGA EL NUEVO TURNO AL HTML
+                let cargando = setTimeout (() => {
+                    try {
+                        //SE INTENTA AGREGAR AL HTML
+                        json.turnos[v_mes].push(turno)
+                        agregar_al_html(turno)
+                        formulario.reset()
+                        modificacion_date()
+                        Swal.fire({
+                            title: "Turno agregado con éxito!",
+                            icon: "success",
+                            draggable: true,
+                            confirmButtonColor: 'rgb(143, 173, 136)'
+                        });
+
+                    } catch (error) {
+                        //SI NO, MUESTRA QUE ERROR HAY
+                        Swal.fire({
+                            icon: "error",
+                            title: "Error: No se ha podido añadir el turno",
+                            text: error,
+                            confirmButtonColor: 'rgb(143, 173, 136)'
+                        });
+                    } finally {
+                        
+                    }
+                    
+                }, 2000)
+
+                //FUNCION PARA AGREGAR EL NUEVO TURNO A HTML
+                function agregar_al_html(turno) {
+                    const turno_agregado = document.getElementById(v_mes + "-dia" + turno.dia)
+
+                    turno_agregado.innerHTML += `
+                    <hr><li>${turno.nom_ape}: De ${turno.entrada} a ${turno.salida}</li>
+                    `;
+                }
+
+                function modificacion_date () {
+                    let fecha_mod = new Date();
+                    label_ultima_mod.innerText = `Ultima modificación: ${fecha_mod}`;
+                }           
             }
 
-            const label_ultima_mod = document.getElementById('label_ultima_mod');
-            label_ultima_mod.innerText = `Agregando..`;
-
-            //SE AGREGA A LA LISTA Y SE AGREGA EL NUEVO TURNO AL HTML
-            let cargando = setTimeout (() => {
-                json.turnos[v_mes].push(turno)
-                agregar_al_html(turno)
-                formulario.reset()
-                modificacion_date()
-                Swal.fire({
-                    title: "Turno agregado con éxito!",
-                    icon: "success",
-                    draggable: true,
-                    confirmButtonColor: 'rgb(143, 173, 136)'
-                });
-            }, 2000)
-
-            
+             
         });
-
-
-
-
-        //FUNCION PARA AGREGAR EL NUEVO TURNO A HTML
-        function agregar_al_html(turno) {
-            const turno_agregado = document.getElementById("dia" + turno.dia)
-
-            turno_agregado.innerHTML += `
-            <hr><li>${turno.nom_ape}: De ${turno.entrada} a ${turno.salida}</li>
-            `;
-        }
-
-        function modificacion_date () {
-            let fecha_mod = new Date();
-            const label_ultima_mod = document.getElementById('label_ultima_mod');
-            label_ultima_mod.innerText = `Ultima modificación: ${fecha_mod}`;
-        }
-
 
     })
     .catch(error => {
